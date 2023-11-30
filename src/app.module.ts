021ee83e9bb73231bec7d11600f07097d229e6b4
Vrunda <vrunda1002@gmail.com>
2023-11-30 12:05:13 +0530

    Inital setup

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [ReportsModule, UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
