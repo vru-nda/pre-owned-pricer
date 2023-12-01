6dcb45a1fb88eb9551b521b4aa82f11498c8100b
Vrunda <vrunda1002@gmail.com>
2023-12-01 19:07:31 +0530

    Signup and singin added

---
+++ src/users/auth.service.ts
@@ -0,0 +1,45 @@
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { scrypt as _scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async singup(email: string, password: string) {
+    const users = await this.usersService.find(email);
+    if (users?.length > 0) {
+      throw new BadRequestException('Email is already in use.');
+    }
+
+    // Hash the password
+    const salt = randomBytes(8).toString('hex');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.usersService.create(email, result);
+    return user;
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+
+    if (!user) {
+      throw new NotFoundException('Invalid Email or Password.');
+    }
+
+    const [salt, storedHash] = user.password.split('.');
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('Invalid Email or Password');
+    }
+    return user;
+  }
+}
