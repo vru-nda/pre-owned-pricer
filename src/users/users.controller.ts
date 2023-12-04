4e71715e5ec515d7bbc75db59f3d6938193e184f
Vrunda <vrunda1002@gmail.com>
2023-12-04 15:48:10 +0530

    Added guards

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,7 +9,9 @@ import {
   Post,
   Query,
   Session,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
 import { Serialize } from 'src/interceptors/serialize.Interceptor';
 import { AuthService } from './auth.service';
 import { CurrentUser } from './decorators/current-user.decorator';
@@ -28,6 +30,7 @@ export class UsersController {
   ) {}
 
   @Get('/currentUser')
+  @UseGuards(AuthGuard)
   getCurrentUser(@CurrentUser() user: User) {
     return user;
   }
