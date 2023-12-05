715fe96d3126a9a6720371ef4ea27d5a7c0bf5fe
Vrunda <vrunda1002@gmail.com>
2023-12-05 11:38:29 +0530

    Mock implementation for the testing

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,42 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { UsersService } from './users.service';
+
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    // create a fake copy of usersService
+    const fakeUsersService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
+
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
+
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.singup('test@new.com', 'password');
+
+    expect(user.password).not.toEqual('test@new.com');
+    const [salt, hashed] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hashed).toBeDefined();
+  });
+});
