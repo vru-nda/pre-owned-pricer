fd64f3ae2862ff0ef352c1dd3f091762fa20ad91
Vrunda <vrunda1002@gmail.com>
2023-12-06 14:43:09 +0530

    Validations for the query params

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AdminGuard } from 'src/guards/admin.guard';
@@ -13,6 +15,7 @@ import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 
@@ -25,6 +28,12 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
     return this.reportsService.create(body, user);
+  }
+
+  @Get()
+  @UseGuards(AuthGuard)
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log('getEstimate called', query);
   }
 
   @Patch('/:id')
